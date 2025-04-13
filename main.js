139c226d12877b30c42bbeb4f7e4d5e8218007b0
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-04-13 00:15:01 -0600

    turn tank

--- main.js
+++ main.js
@@ -5,14 +5,38 @@ const ctx = canvas.getContext("2d")
 const square1Size = 50
 let angle = 0
 
-ctx.strokeRect(0, 0, canvas.width, canvas.height)
+const tank1 = { x: 100, y: 100, angle: 0, color: "blue" }
 
-ctx.translate(square1Size * 2, square1Size * 2)
-ctx.fillRect(0, 0, 4, 4)
+function thank(tank) {
+  ctx.save()
+  ctx.translate(tank.x, tank.y)
+  ctx.rotate(tank.angle)
+  ctx.fillStyle = "tank.color"
+  ctx.fillRect(-square1Size / 2, -square1Size / 2, square1Size, square1Size)
 
-ctx.strokeRect(-square1Size / 2, -square1Size / 2, square1Size, square1Size)
+  //ctx.strokeStyle = tank.color
+  ctx.beginPath()
+  ctx.moveTo(0, 0)
+  ctx.lineTo(square1Size, 0)
+  ctx.stroke()
 
-ctx.beginPath()
-ctx.moveTo(0, 0)
-ctx.lineTo(square1Size, 0)
-ctx.stroke()
+  ctx.restore()
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.strokeRect(0, 0, canvas.width, canvas.height)
+  thank(tank1)
+
+  requestAnimationFrame(gameLoop)
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowDown") tank1.angle = Math.PI / 2
+  if (e.key === "ArrowLeft") tank1.angle = Math.PI
+  if (e.key === "ArrowUp") tank1.angle = Math.PI * 1.5
+  if (e.key === "ArrowRight") tank1.angle = 0
+  gameLoop()
+})
+
+gameLoop()
